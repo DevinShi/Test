ace5dd29d18fb6cdfe4a89b4fb1ce3e81b27d9b4
shihaibo <shihaibo@shihaibodemacbook-pro.local>
2016-05-14 18:07:52 +0800

    初始化项目

---
+++ TestWithGitHub/TestWithGitHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestWithGitHub
+//
+//  Created by shihaibo on 16/5/14.
+//  Copyright © 2016年 OCJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestWithGitHub/TestWithGitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestWithGitHub
+//
+//  Created by shihaibo on 16/5/14.
+//  Copyright © 2016年 OCJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
